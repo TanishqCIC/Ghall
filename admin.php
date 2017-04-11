05ecd874217fbb2fcbbd17b8f5b65ea94f0ef616
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-04-11 23:23:37 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -38,3 +38,73 @@
     </div>
   </div>
 </div>
+
+<section id="explore">
+		<div class="container">
+			<div class="row">
+				<div class="watch">
+					<img class="img-responsive" src="images/people.png" alt="">
+				</div>				
+				<div class="col-md-7 col-md-offset-4 col-sm-5">
+				<div class="admin">
+					<h2>Administration</h2>
+					<p>The Hostel has excellent residential facilities for the students admitted to the various courses. Hostel administration is managed by a Provost who is assisted by a Warden and a Resident tutor drawn from the Delhi University faculty on a tenure basis. </p>
+
+					<p>A Managing committee under the overall guidelines of the policies of the University of Delhi is responsible for major policy decisions with respect to the hostel administration. Hostel Executive Committee takes decisions on routine matters. Students have representation on in the executive Committee. Hostel office has full-fledged supporting staff and other workers.</p>
+					<br>
+					<br>
+					<br>
+					<br>
+
+				</div>				
+				</div>
+				<div class="col-sm-7 col-md-12">					
+					<ul id="countdown">
+						<li>					
+							<p>Chairman</p>
+							<img src="images/cm.jpg" width="80%"></img><br>
+							<a>Prof. M.L. Singla</a><br>
+							<a class="post">Faculty of Management Studies</a><br>
+							<a class="post">University of Delhi</a><br>
+						</li>
+						<li>					
+							<p>Provost</p>
+							<img src="images/rsz_pv.jpg" width="98%"><br>
+							<a>Prof. R.K. Sharma</a><br>
+							<a class="post">Department of Chemistry</a><br>
+							<a class="post">University of Delhi</a><br>
+							
+						</li>
+						<li>					
+							<p>Warden</p>
+							<img src="images/rsz_wd.jpg" width="80%"><br>
+							<a>Prof. M.L. Singla</a><br>
+							<a class="post">Faculty of Management Studies</a><br>
+							<a class="post">University of Delhi</a><br>
+						</li>
+						<li>					
+							<p>Resident Tutor</p>
+							<img src="images/cm.jpg" width="80%"><br>
+							<a>Prof. M.L. Singla</a><br>
+							<a class="post">Faculty of Management Studies</a><br>
+							<a class="post">University of Delhi</a><br>
+						</li>
+						
+					</ul>
+				</div>
+			</div>
+			
+			</div>
+		</div>
+	</section><!--/#explore-->
+	<br>
+	<br>
+	<br>
+	<br>
+	<div class="navbar navbar-fixed-bottom opaque-navbar">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navMain">
+  <span class="glyphicon glyphicon-chevron-right" style="color:white;"></span>
+    
+  </button>

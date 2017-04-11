81509e7efd737e33712069290a61830616999fde
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-04-11 23:22:39 +0530

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<head>
+<title>Administration | Gwyer Hall</title>
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<!--<link href="css/main_typed.css" rel="stylesheet">-->
+<link href="css/font-awesome.min.css" rel="stylesheet">
+<link href="css/responsive.css" rel="stylesheet">
+<link href="css/slider.css" rel="stylesheet">
+<link href="css/admin.css" rel="stylesheet">
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/jquery.nav.js"></script>
+</head>
+
+<body>
+
+<div class="navbar navbar-fixed-top opaque-navbar">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navMain">
+  <span class="glyphicon glyphicon-chevron-right" style="color:white;"></span>
+    
+  </button>
+      
+    </div>
+    <div class="collapse navbar-collapse" id="navMain">
+      <ul class="nav navbar-nav pull-right">
+        <li><a href="index.php">Home</a></li>
+        <li class="active"><a href="#">Administration</a></li>
+        <li><a href="admission.php">Admission</a></li>
+        <li><a href="facilities.php">Facilities</a></li>
+        <li><a href="alumni.php">Alumni</a></li>
+        <li><a href="#">Residency</a></li> 
+        <li><a href="#">Contact Us</a></li> 
+      </ul>
+    </div>
+  </div>
+</div>

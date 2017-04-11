f8cea6223bf2dde05a725114e1f5df888717c6fb
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-04-11 23:25:52 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -108,3 +108,32 @@
   <span class="glyphicon glyphicon-chevron-right" style="color:white;"></span>
     
   </button>
+      
+    </div>
+    <div class="collapse navbar-collapse" id="navMain" style="text-align: center">
+    
+      Designed and developed by <span style="color:yellow">Tanishq Kumar Dhangar</span>
+   <p>Copyright 2017</p>
+    </div>
+  </div>
+</div>
+<script>
+   /*
+  **********************************************************
+  * OPAQUE NAVBAR SCRIPT
+  **********************************************************
+  */
+
+  // Toggle tranparent navbar when the user scrolls the page
+
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ 
+    {
+        $('.opaque-navbar').addClass('opaque');
+    } else {
+        $('.opaque-navbar').removeClass('opaque');
+    }
+});
+</script>
+</body>
+</html>

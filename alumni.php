8fd3eb06947ee3890198ef9c9faa9a6eb592dc11
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-04-15 21:07:34 +0530

    Update alumni.php

--- alumni.php
+++ alumni.php
@@ -40,3 +40,69 @@
 </div>
 <br>
 <br>
+
+<div class="alumni">
+                    <img class="img-responsive" src="images/icon_alumni.png" alt="">
+                </div>              
+<div class="container_form_alumni"> 
+      <div class="row">
+        <div class="container">
+            <div class="login-register-form-section">
+                <ul class="nav nav-tabs" role="tablist">
+                	<li class="active"><a href="#instructions" data-toggle="tab">Search your Name</a></li>
+                    <li><a href="#register" data-toggle="tab">Register</a></li>
+
+                </ul>
+                <div class="tab-content">
+                	<div role="tabpanel" class="tab-pane fade in active" id="instructions">
+                	If you are not listed in our database, please register yourself by clicking on the "Register" button above.<br>
+                	<br>
+                    <form class="form-horizontal" method="post" action="">
+                            <div class="form-group " >
+                                <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-user"></i></div>
+                                    <input type="text" name="login_email" class="form-control" placeholder="Username or email" required="required" value="">
+                                </div>
+                            </div>
+                            <input type="submit" value="Search" class="btn btn-success btn-custom">
+                	</div>
+                    
+                    <div role="tabpanel" class="tab-pane fade" id="register">
+                        <form class="form-horizontal" method="post" action="">
+                            <div class="form-group ">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-user"></i></div>
+                                    <input type="text" name="register_username" class="form-control" placeholder="Username" required="required" value="">
+                                </div>
+                            </div>
+                            <div class="form-group ">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-male"></i></div>
+                                    <input type="text" name="register_fullname" class="form-control" placeholder="Full name" required="required" value="">
+                                </div>
+                            </div>
+                            <div class="form-group ">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-envelope"></i></div>
+                                    <input type="email" name="register_email" class="form-control" placeholder="Email" required="required" value="">
+                                </div>
+                            </div>
+                            <div class="form-group ">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-lock"></i></div>
+                                    <input type="password" name="register_password" class="form-control" placeholder="Password" required="required">
+                                </div>
+                            </div>
+                            <div class="form-group ">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-lock"></i></div>
+                                    <input type="password" name="register_cpassword" class="form-control" placeholder="Confirm Password" required="required">
+                                </div>
+                            </div>
+                            <input type="submit" value="Register" class="btn btn-success btn-custom">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>

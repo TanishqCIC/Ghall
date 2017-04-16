3e56f3a9c6d915e1b0208c18d53ba0a146f93d2a
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-04-16 21:11:30 +0530

    Update form.php

--- form.php
+++ form.php
@@ -43,3 +43,92 @@ $phone = $_SESSION['phone'];
 <br>
 
 
+<div class="container_form_alumni"> 
+      <div class="row">
+        <div class="container">
+            <div class="login-register-form-section">
+            <br>
+            <br>
+            <br>
+                <ul class="nav nav-tabs" role="tablist">
+                	<li class="active"><a href="#instructions" data-toggle="tab">Instructions</a></li>
+                    <li><a href="#register" data-toggle="tab">Form</a></li>
+
+                </ul>
+                <div class="tab-content">
+                	<div role="tabpanel" class="tab-pane fade in active" id="instructions">
+                	1. You can fill the form in various attempts, provided you 'SAVE' it every time before you logout.<br>
+                	2. You CANNOT edit form once you SUBMIT it !
+                	<br>
+                    </div>
+                  
+                    <div role="tabpanel" class="tab-pane fade" id="register">
+                        <form class="form-horizontal" method="post" action="">
+                            <div class="form-group ">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-user"></i></div>
+                                    <input type="text" name="name" class="form-control" placeholder="Name of Candidate" required="required" value="">
+                                </div>
+                            </div>
+                            <div class="form-group ">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-male"></i></div>
+                                    <input type="dropdown" name="fathername" class="form-control" placeholder="Father's name" required="required" value="">
+                                </div>
+                            </div>
+                            <div class="form-group ">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-female"></i></div>
+                                    <input type="dropdown" name="fathername" class="form-control" placeholder="Mother's name" required="required" value="">
+                                </div>
+                            </div>
+                            <div class="form-group ">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-envelope"></i></div>
+                                    <input type="email" name="register_email" class="form-control" placeholder="Email" required="required" value="<?php echo $mail?>">
+                                </div>
+                            </div>
+                            <div class="form-group ">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-phone"></i></div>
+                                    <input type="mobile" name="mobile" class="form-control" placeholder="Contact No. (Mobile)" required="required" value="<?php echo $phone?>">
+                                </div>
+                            </div>
+                            <div class="form-group ">
+                                <div class="input-group">
+                                    <div class="input-group-addon"><i class="fa fa-phone"></i></div>
+                                    <input type="phone" name="phone" class="form-control" placeholder="Additional Phone no.">
+                                </div>
+                            </div>
+                            <div class="form-group ">
+                                <div class="input-group">
+                                     <select>
+                                    	<option class="form-control" value="" selected disabled> Select level of Academia</option>
+                                    	<option>Undergraduation</option>
+                                    	<option>Postgraduation</option>
+                                    	<option>Doctorate (PhD)</option>
+                                    	<option>Post Doctorate</option>
+                                    	<option>Teacher</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group ">
+                                <div class="input-group">
+                                     <select>
+                                    	<option class="form-control" value="" selected disabled> Select Department</option>
+                                    	<option>Cluster Innovation Centre (CIC)</option>
+                                    	<option>Campus Law Centre (CLC)</option>
+                                    	<option>Department of English</option>
+                                    	<option>Department of Hindi</option>
+                                    	<option>Department of Mathematics</option>
+                                    	<option>Department of Computer Science (DUCS)</option>
+                                    	<option>Department of Sanskrit</option>
+                                    	<option>Department of Geography</option>
+                                    	<option>Department of History</option>
+                                    	<option>Department of Political Science</option>
+                                    	<option>Department of Physics</option>
+                                    	<option>Department of Chemistry</option>
+                                    	<option>Department of Zoology</option>
+                                    </select>
+                                </div>
+                            </div>

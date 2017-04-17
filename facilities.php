6d56ed831b7c70d72a1422c817e553eb17ca3546
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-04-17 21:15:51 +0530

    Update facilities.php

--- facilities.php
+++ facilities.php
@@ -1,2 +1,17 @@
-<?php
-?>
+<!doctype html>
+<head>
+<title>Facilities | Gwyer Hall</title>
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<!--<link href="css/main_typed.css" rel="stylesheet">-->
+<link href="css/font-awesome.min.css" rel="stylesheet">
+<link href="css/responsive.css" rel="stylesheet">
+<link href="css/slider.css" rel="stylesheet">
+<link href="css/facilities.css" rel="stylesheet">
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/jquery.nav.js"></script>
+</head>
+
+<body>

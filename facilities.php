fb6d3e5c4afbf29ab77e1770c03771aaa3872c8c
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-04-17 21:16:24 +0530

    Update facilities.php

--- facilities.php
+++ facilities.php
@@ -15,3 +15,54 @@
 </head>
 
 <body>
+
+<div class="navbar navbar-fixed-top opaque-navbar">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navMain">
+  <span class="glyphicon glyphicon-chevron-right" style="color:white;"></span>
+    
+  </button>
+      
+    </div>
+    <div class="collapse navbar-collapse" id="navMain">
+      <ul class="nav navbar-nav pull-right">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="admin.php">Administration</a></li>
+        <li><a href="admission.php">Admission</a></li>
+        <li class="active"><a href="#">Facilities</a></li>
+        <li><a href="alumni.php">Alumni</a></li>
+        <li><a href="#">Residency</a></li> 
+        <li><a href="#">Contact Us</a></li> 
+  </ul>
+    </div>
+  </div>
+</div>
+
+<section id="explore">
+		<div class="container">
+			<div class="row">
+				<div class="watch">
+					<img class="img-responsive" src="images/icons_facilities.png" alt="">
+				</div>				
+				<div class="col-md-7 col-md-offset-4 col-sm-5">
+				<div class="admin">
+					<h2>Facilities</h2>
+					<p>Gwyer Hall is the oldest and most prestigeous Mens' Hostel in Delhi University. These facilities are available in Gwyer Hall... </p>
+
+					<p></p>
+					<br>
+					<br>
+					<br>
+					<br>
+					<br>
+					<br>
+					<br>
+					<br>
+				</div>				
+				</div>
+				<div class ="row">
+				<div class="col-sm-4" id="ptext">
+				<p><h2>Dining Hall</h2></p>
+							<br>
+

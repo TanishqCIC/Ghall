6261b5ef73926470a57d35d2e460706b8be205a1
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-04-19 19:44:30 +0530

    Update pdf.php

--- pdf.php
+++ pdf.php
@@ -1,2 +1,26 @@
-<?
-?>
+<?php 
+
+require('fpdf18/fpdf.php'); 
+
+
+
+
+class PDF extends FPDF
+{
+// Page header
+function Header()
+{
+    // Logo
+  #  $this->Image('logo.png',10,6,30);
+    // Arial bold 15
+    $pdf = new FPDF();
+
+	$this->AddFont('ATOldEnglish', '', 'ATOldEnglish.php');
+    $this->SetFont('ATOldEnglish','',30);
+    // Move to the right
+    $this->Cell(80);
+    // Title
+    $this->Cell(30,10,'Gwyer Hall',0,0,'C');
+    // Line break
+    $this->Ln(12);
+    $this->SetFont('ATOldEnglish','',15);

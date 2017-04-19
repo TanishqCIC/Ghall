d2b5f5322567014cf76f4be8df881ec6a3fad978
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-04-19 19:44:52 +0530

    Update pdf.php

--- pdf.php
+++ pdf.php
@@ -24,3 +24,20 @@ function Header()
     // Line break
     $this->Ln(12);
     $this->SetFont('ATOldEnglish','',15);
+    $this->Cell(80);
+    $this->Cell(30,10,'University of Delhi',0,0,'C');
+    // Line break
+    $this->Ln(20);
+}
+
+// Page footer
+function Footer()
+{
+    // Position at 1.5 cm from bottom
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial','I',8);
+    // Page number
+    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+}
+}
